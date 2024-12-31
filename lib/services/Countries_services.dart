660f2e6d7ed09c08f867908060b8aa7ec4671b05
Murtaza lonsara <88498449+lonsara@users.noreply.github.com>
2024-12-31 12:16:36 +0530

    updated

--- lib/services/Countries_services.dart
+++ lib/services/Countries_services.dart
@@ -1,10 +1,11 @@
 import 'dart:convert';
+import 'package:currency_converter/services/utils/apis.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
   class CountriesServices {
   // Replace 'YOUR_API_URL' with the actual URL of your API
   Future<Map<String, dynamic>> getdatafromAPI() async {
-  final response = await http.get(Uri.parse('https://api.currencyapi.com/v3/latest?apikey=cur_live_NMVEfB9BHwupH6kVHkuEOwNc4QibLdyObmJh215E'));
+  final response = await http.get(Uri.parse(Apis.baseapi));
 
   if (response.statusCode == 200) {
   // Decode the JSON response
--- lib/services/utils/apis.dart
+++ lib/services/utils/apis.dart
@@ -1,4 +1,4 @@
 class Apis{
-  static const String baseapi='https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/';
+  static const String baseapi='https://api.currencyapi.com/v3/latest?apikey=cur_live_NMVEfB9BHwupH6kVHkuEOwNc4QibLdyObmJh215E';
   static const String countries='${baseapi}currencies.json';
 }
